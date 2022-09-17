8ddfe2ddd484fda03b3cf1acbe26ba783f7835db
Abram <alenso8@ya.ru>
2022-09-17 20:22:07 +0300

    versd

---
+++ Band_if/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите ряд чисел, разделенных запятой : ");
+            string seriesOfNumbers = Console.ReadLine();
+
+            seriesOfNumbers = seriesOfNumbers + ",";    
+
+          
+       
+
+            string seriesNew = RemovingSpaces(seriesOfNumbers);
+
+            int[] arrayOfNumbers = ArrayOfNumbers(seriesNew);
+
+            PrintArry(arrayOfNumbers);
+
+
