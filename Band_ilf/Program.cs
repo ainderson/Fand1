9fc0d172be80eede7176258820b489b7e9815330
Abram <alenso8@ya.ru>
2022-09-17 20:11:35 +0300

    Version4

---
+++ Band_ilf/Program.cs
@@ -0,0 +1,14 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+
+Console.WriteLine("Введите число");
+           int c = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+
+            while (c > 0)
+            {
+                int num = c % 10;
+                c = c / 10;
+                sum = sum + num;
+            }
+            Console.WriteLine("сумма всех цифр в числе равна: " + sum);
